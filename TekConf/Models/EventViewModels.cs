e3efce13badf38a5d41d46cfad1a1b67bfeabbc4
Camille Dollé <dollecamille@gmail.com>
2015-06-24 21:51:56 +0200

    Details page done

--- TekConf/Models/EventViewModels.cs
+++ TekConf/Models/EventViewModels.cs
@@ -10,4 +10,12 @@ namespace TekConf.Models
         public Event Event { get; set; }
         public List<Technology> technologies { get; set; }
     }
+
+    public class EventDetailsViewModel
+    {
+        public Event Event { get; set; }
+        public List<AspNetUsers> speakers { get; set; }
+        public List<AspNetUsers> attendees { get; set; }
+        public List<Technology> technologies { get; set; }
+    }
 }
