622259f9dec06d7e1c2a0788a181104ead8276ef
Camille Dollé <dollecamille@gmail.com>
2015-06-24 15:05:06 +0200

    added edmx file

--- TekConf/Models/IdentityModels.cs
+++ TekConf/Models/IdentityModels.cs
@@ -26,7 +26,7 @@ namespace TekConf.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("IdentityConnection", throwIfV1Schema: false)
         {
         }
 
